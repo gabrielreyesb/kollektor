adf72080e54056743cfd36c6ff31be9dad74d762
Gabriel Reyes <gabriel@handy.la>
2024-12-30 13:34:04 -0600

    Fix controller imports

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,13 +1,13 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
+// Import and register all your controllers from the controllers directory
 import { application } from "./application"
-import FilterController from "./filter_controller"
 import AlertController from "./alert_controller"
+import FilterController from "./filter_controller"
 import SidebarController from "./sidebar_controller"
 import DisplayController from "./display_controller"
 import GenreFilterController from "./genre_filter_controller"
 
-application.register("filter", FilterController)
 application.register("alert", AlertController)
+application.register("filter", FilterController)
 application.register("sidebar", SidebarController)
 application.register("display", DisplayController)
 application.register("genre-filter", GenreFilterController)
--- config/importmap.rb
+++ config/importmap.rb
@@ -12,3 +12,14 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 # Add Bootstrap and its dependencies
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
+
+# Explicitly pin controllers
+pin "controllers/application", to: "controllers/application.js"
+pin "controllers/alert_controller", to: "controllers/alert_controller.js"
+pin "controllers/filter_controller", to: "controllers/filter_controller.js"
+pin "controllers/sidebar_controller", to: "controllers/sidebar_controller.js"
+pin "controllers/display_controller", to: "controllers/display_controller.js"
+pin "controllers/genre_filter_controller", to: "controllers/genre_filter_controller.js"
+
+# Keep this at the end
+pin_all_from "app/javascript/controllers", under: "controllers"
