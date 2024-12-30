a35308d3c1646e5142d78c70167c2a32174de142
Gabriel Reyes <gabriel@handy.la>
2024-12-30 13:20:41 -0600

    Fix Stimulus controller loading

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -11,3 +11,7 @@ application.register("alert", AlertController)
 application.register("sidebar", SidebarController)
 application.register("display", DisplayController)
 application.register("genre-filter", GenreFilterController)
+
+// Eager load all controllers defined in the import map under controllers/**/*_controller
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+eagerLoadControllersFrom("controllers", application)
