ee7c557f8477922b43487634ee6aca923e246d50
Gabriel Reyes <gabriel@handy.la>
2024-12-30 13:26:52 -0600

    Fix import paths

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,7 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 # Pin your custom JavaScript modules
 pin "application", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+# Add Bootstrap and its dependencies
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
