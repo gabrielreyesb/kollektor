cf1a16c557b4ed7f52da39eed44e0992fd7765ac
Gabriel Reyes <gabriel@handy.la>
2025-01-05 20:07:55 -0600

    Show missing images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,29 +5,21 @@ Kollektor::Application.configure do
   config.eager_load = true
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
-  
-  # Asset handling
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.assets.compile = true
-  config.assets.digest = true
-  config.assets.css_compressor = nil
-  
-  # Active Storage configuration
   config.active_storage.service = :local
-  
-  # SSL configuration
   config.force_ssl = true
   
-  # Logging configuration
   config.logger = ActiveSupport::Logger.new(STDOUT)
     .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
     .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
+
   config.log_tags = [ :request_id ]
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
-  
-  # Other configurations
   config.action_mailer.perform_caching = false
   config.i18n.fallbacks = true
   config.active_support.report_deprecations = false
   config.active_record.dump_schema_after_migration = false
+
+  # Enable serving of static files
+  config.public_file_server.enabled = true
 end
