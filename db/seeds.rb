adbd9890a4641b8f5deb952f013d76993eb29d63
Gabriel Reyes <gabriel@handy.la>
2025-01-05 19:14:09 -0600

    Add countries seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,30 @@
 #   end
 
 require_relative 'seeds/countries'
+
+# Countries seed data
+countries = [
+  { name: 'United States', code: 'US' },
+  { name: 'United Kingdom', code: 'UK' },
+  { name: 'Canada', code: 'CA' },
+  { name: 'Australia', code: 'AU' },
+  { name: 'Germany', code: 'DE' },
+  { name: 'France', code: 'FR' },
+  { name: 'Spain', code: 'ES' },
+  { name: 'Italy', code: 'IT' },
+  { name: 'Japan', code: 'JP' },
+  { name: 'Brazil', code: 'BR' },
+  { name: 'Mexico', code: 'MX' },
+  { name: 'Argentina', code: 'AR' },
+  { name: 'Sweden', code: 'SE' },
+  { name: 'Netherlands', code: 'NL' },
+  { name: 'Ireland', code: 'IE' }
+]
+
+# Create countries if they don't exist
+countries.each do |country_data|
+  Country.find_or_create_by!(name: country_data[:name]) do |country|
+    country.code = country_data[:code]
+    puts "Created country: #{country.name}"
+  end
+end
