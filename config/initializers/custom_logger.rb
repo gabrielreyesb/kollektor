33c804c87c825d463cd5a9edcc8378a1879514ea
Gabriel Reyes <gabriel@handy.la>
2025-01-27 16:45:49 -0600

    Fix custom logger for production

--- config/initializers/custom_logger.rb
+++ config/initializers/custom_logger.rb
@@ -1,6 +1,5 @@
-logfile = File.open(Rails.root.join('log', 'debug.log'), 'a')
-logfile.sync = true
-DEBUG_LOGGER = Logger.new(logfile)
-DEBUG_LOGGER.formatter = proc do |severity, datetime, progname, msg|
-  "#{datetime}: #{severity} -- #{msg}\n"
+if Rails.env.development?
+  # Only create debug log in development
+  debug_logger = Logger.new(Rails.root.join('log', 'debug.log'))
+  Rails.logger.extend(ActiveSupport::Logger.broadcast(debug_logger))
 end 
