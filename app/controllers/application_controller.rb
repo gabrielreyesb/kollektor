2f32bbc48d81520e66239e4c67ef4dd42b585142
Gabriel Reyes <gabriel@handy.la>
2025-01-27 16:49:57 -0600

    Fix logger for production environment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,11 @@ class ApplicationController < ActionController::Base
     
     # Filter authors if genre is selected
     @authors = if params[:genre_id].present?
-      DEBUG_LOGGER.info "Genre ID received: #{params[:genre_id]}"
+      Rails.logger.debug "Genre ID received: #{params[:genre_id]}"
       
       # Debug counts before the join
-      DEBUG_LOGGER.info "Authors with this genre: #{Author.where(genre_id: params[:genre_id]).count}"
-      DEBUG_LOGGER.info "Albums with this genre: #{Album.where(genre_id: params[:genre_id]).count}"
+      Rails.logger.debug "Authors with this genre: #{Author.where(genre_id: params[:genre_id]).count}"
+      Rails.logger.debug "Albums with this genre: #{Album.where(genre_id: params[:genre_id]).count}"
       
       query = Author.left_joins(:albums)
                    .where("authors.genre_id = :genre_id OR albums.genre_id = :genre_id", 
@@ -20,14 +20,14 @@ class ApplicationController < ActionController::Base
                    .distinct
                    .order(:name)
       
-      DEBUG_LOGGER.info "SQL Query: #{query.to_sql}"
+      Rails.logger.debug "SQL Query: #{query.to_sql}"
       result = query.to_a
-      DEBUG_LOGGER.info "Results count: #{result.size}"
-      DEBUG_LOGGER.info "Author IDs: #{result.map(&:id)}"
+      Rails.logger.debug "Results count: #{result.size}"
+      Rails.logger.debug "Author IDs: #{result.map(&:id)}"
       
       result
     else
-      DEBUG_LOGGER.info "No genre ID present, showing all authors"
+      Rails.logger.debug "No genre ID present, showing all authors"
       Author.order(:name)
     end
     
